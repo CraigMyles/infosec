ee87b2c11200c2840f0b351bfc1ad6e0b63e86fd
BuildTools <unconfigured@null.spigotmc.org>
2018-11-06 20:15:44 +0000

    adjusted the required include files

--- infosecQuiz/authorisationMenu.cs
+++ infosecQuiz/authorisationMenu.cs
@@ -1,15 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Management;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace infosecQuiz
